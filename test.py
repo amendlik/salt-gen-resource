ad5db11c8a9323d71a8edae9c9b9a88a4313ee2b
Adam Mendlik <amendlik@gmail.com>
2016-10-29 08:21:32 -0600

    Add attribute and tag tests

--- test.py
+++ test.py
@@ -6,42 +6,100 @@ else:
 from SaltGenResource import ResourceGenerator
 
 
-class TestResourceGenerator(unittest.TestCase):
+class TestNodeGenerator(unittest.TestCase):
 
+    _base_args = []
     required_attributes = ['hostname', 'osArch', 'osFamily',
                            'osName', 'osVersion']
 
     def test_glob_targeting(self):
-        args = ['*']
+        args = self._base_args + ['*']
         resources = ResourceGenerator(args).get_nodes()
-        self._test_required_attributes(resources)
+        self._test_attributes(resources, self.required_attributes)
 
     def test_cidr_targeting(self):
-        args = ['-S', '0.0.0.0/0']
+        args = self._base_args + ['-S', '0.0.0.0/0']
         resources = ResourceGenerator(args).get_nodes()
-        self._test_required_attributes(resources)
+        self._test_attributes(resources, self.required_attributes)
 
     def test_grain_targeting(self):
-        args = ['-G', 'os:*']
+        args = self._base_args + ['-G', 'os:*']
         resources = ResourceGenerator(args).get_nodes()
-        self._test_required_attributes(resources)
+        self._test_attributes(resources, self.required_attributes)
 
     def test_pcre_targeting(self):
-        args = ['-E', '.*']
+        args = self._base_args + ['-E', '.*']
         resources = ResourceGenerator(args).get_nodes()
-        self._test_required_attributes(resources)
+        self._test_attributes(resources, self.required_attributes)
 
     def test_grain_pcre_targeting(self):
-        args = ['-P', 'os:.*']
+        args = self._base_args + ['-P', 'os:.*']
         resources = ResourceGenerator(args).get_nodes()
-        self._test_required_attributes(resources)
+        self._test_attributes(resources, self.required_attributes)
 
-    def _test_required_attributes(self, resources):
+    def test_single_attribute(self):
+        optional_attributes = ['os']
+        args = self._base_args + ['-a', optional_attributes[0], '*']
+        resources = ResourceGenerator(args).get_nodes()
+        self._test_attributes(resources, self.required_attributes)
+        self._test_attributes(resources, optional_attributes)
+
+    def test_multiple_attributes1(self):
+        optional_attributes = ['os', 'os_family']
+        args = self._base_args + ['-a', ' '.join(optional_attributes), '*']
+        resources = ResourceGenerator(args).get_nodes()
+        self._test_attributes(resources, self.required_attributes)
+        self._test_attributes(resources, optional_attributes)
+
+    def test_multiple_attributes2(self):
+        optional_attributes = ['os', 'os_family']
+        args = self._base_args + ['-a', ','.join(optional_attributes), '*']
+        resources = ResourceGenerator(args).get_nodes()
+        self._test_attributes(resources, self.required_attributes)
+        self._test_attributes(resources, optional_attributes)
+
+    def test_single_tag(self):
+        tags = ['os']
+        args = self._base_args + ['-t', tags[0], '*']
+        resources = ResourceGenerator(args).get_nodes()
+        self._test_attributes(resources, self.required_attributes)
+        self._test_tags(resources, tags)
+
+    def test_multiple_tags1(self):
+        tags = ['os', 'os_family']
+        args = ['-t', ' '.join(tags), '*']
+        resources = ResourceGenerator(args).get_nodes()
+        self._test_attributes(resources, self.required_attributes)
+        self._test_tags(resources, tags)
+
+    def test_multiple_tags1(self):
+        tags = ['os', 'os_family']
+        args = self._base_args + ['-t', ','.join(tags), '*']
+        resources = ResourceGenerator(args).get_nodes()
+        self._test_attributes(resources, self.required_attributes)
+        self._test_tags(resources, tags)
+
+    def _test_attributes(self, resources, needed):
         for host, attributes in resources.iteritems():
-            for attribute in self.required_attributes:
+            for attribute in needed:
                 self.assertIn(attribute, attributes)
                 self.assertIsNotNone(attributes[attribute])
                 self.assertNotEqual(attributes[attribute], '')
+
+    def _test_tags(self, resources, needed):
+        for host, attributes in resources.iteritems():
+            self.assertIn('tags', attributes)
+            self.assertIsNotNone(attributes['tags'])
+            self.assertTrue(len(attributes['tags']) >= len(needed))
+
+class TestServerNodeGenerator(TestNodeGenerator):
+    _base_args = ['--include-server-node']
+
+    def _test_attributes(self, resources, needed):
+        super(TestServerNodeGenerator, self)._test_attributes(resources, needed)
+        self.assertIn('localhost', resources)
+        self.assertEqual(resources['localhost']['hostname'], 'localhost')
+
 
 if __name__ == '__main__':
     unittest.main()
